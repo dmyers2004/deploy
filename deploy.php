790a11cf96f29089425344a4bb2f23e2d004c0cf
Don Myers <donmyers@quadratec.com>
2018-06-09 13:23:17 -0400

    updates now with options!

--- deploy.php
+++ deploy.php
@@ -1,27 +1,8 @@
 #!/usr/bin/env php
 <?php
 /*
-// comment
-// % used in the description
-command
-set name value
-capture name shell commands
-import filename (return php array ie. .env)
-sudo on
-sudo off
-task name
-
-gitx show
-gitx status
-gitx update
-selfupdate
-self-update
-
-Most common Bash date commands for timestamping
+Most common Bash date commands for time stamping
 https://zxq9.com/archives/795
-
-git clone https://github.com/dmyers2004/deploy.git ./deploy;chmod 755 ./deploy
-
 */
 
 ini_set('memory_limit','512M');
@@ -30,34 +11,16 @@ error_reporting(E_ALL ^ E_NOTICE);
 
 $config = [
 	'version'=>'4.0.0',
-	'deploy_filename'=>'deploy.json',
+	'deploy_file'=>getcwd().'/deploy.json',
 	'rootpath'=>getcwd(),
 	'env'=>$_SERVER + $_ENV,
 	'args'=>$_SERVER['argv'],
+	'verbose'=>false,
 ];
 
 $deploy = new deploy($config);
 
-/* get the cli arguments */
-$argv = $_SERVER['argv'];
-
-/* shift off the programs name */
-array_shift($argv);
-
-$task_name = implode(' ',$argv);
-
-/* if the task doesn't show all available tasks and their help */
-if (!$deploy->task_exists($task_name)) {
-	if (!empty($task_name)) {
-		$deploy->e('<red>Task "'.$task_name.'" is not defined.</red>');
-	}
-	$deploy->table($deploy->get_help());
-} else {
-	$deploy->task($task_name);
-}
-
-/* exit clean */
-exit(1);
+exit($deploy->options()->process());
 
 /* finished */
 
@@ -66,10 +29,9 @@ class deploy {
 	public $env = [];
 	public $deploy_json = [];
 	public $switch_storage = [];
-	public $config;
+	public $config = [];
 	public $current_task = null;
 	public $current_line = null;
-	
 
 	public function __construct($config) {
 		$this->config = $config;
@@ -77,8 +39,78 @@ class deploy {
 		$this->heading('Deploy Version '.$this->config['version']);
 
 		$this->env = $this->config['env'];
+	}
 
+	public function options() {
+		/* get the cli arguments */
+		$args = $this->config['args'];
+
+		/* shift off the programs name */
+		array_shift($args);
+
+		/* find the -d change directory first so we can then handle -f different file */
+		foreach ($args as $idx=>$val) {
+			/* different directory */
+			if ($val == '-d') {
+				$dir = $args[$idx+1];
+
+				$this->directory_exists($dir);
+
+				chdir($dir);
+
+				$this->config['rootpath'] = getcwd();
+
+				unset($args[$idx],$args[$idx+1]);
+			}
+		}
+
+		/* now handle verbose and file */
+		foreach ($args as $idx=>$val) {
+			switch ($val) {
+				/* verbose */
+				case '-v':
+					$this->config['verbose'] = true;
+
+					unset($args[$idx]);
+				break;
+				/* different deploy file */
+				case '-f':
+					$deploy_file = $args[$idx+1];
+
+					if (substr($deploy_file,0,1) == '/') {
+						$this->config['deploy_file'] = $deploy_file;
+					} else {
+						$this->config['deploy_file'] = getcwd().'/'.$deploy_file;
+					}
+
+					$this->file_exists($this->config['deploy_file']);
+
+					unset($args[$idx],$args[$idx+1]);
+				break;
+			}
+		}
+
+		/* took care of the options */
+		$this->config['args'] = $args;
+
+		return $this;
+	}
+
+	public function process() {
+		/* get our deploy file */
 		$this->deploy_json = array_merge($this->get_hard_actions(),$this->get_deploy());
+
+		$task_name = implode(' ',$this->config['args']);
+
+		/* if the task doesn't show all available tasks and their help */
+		if (!$this->task_exists($task_name)) {
+			if (!empty($task_name)) {
+				$this->e('<red>Task "'.$task_name.'" is not defined.</red>');
+			}
+			$this->table($this->get_help());
+		} else {
+			$this->task($task_name);
+		}
 	}
 
 	public function run($command) {
@@ -112,39 +144,35 @@ class deploy {
 		}
 	}
 
+	public function task_exists($task_name) {
+		return (array_key_exists($task_name,$this->deploy_json));
+	}
+
+	public function selfupdate() {
+		$this->self_update();
+	}
+
+	public function self_update() {
+		$this->heading('Updating Self');
+
+		exec('sudo rm -fdrv /tmp/deploy');
+		exec('sudo git clone https://github.com/dmyers2004/deploy.git /tmp/deploy');
+		exec('sudo mv /tmp/deploy/deploy.php '.__FILE__);
+		exec('sudo chmod -v 755 '.__FILE__);
+
+		$this->sub_heading('Update Complete');
+	}
+
 	public function cli($command) {
 		$exit_code = 0;
 
 		$cli = $this->merge($command);
 
-		$this->e('<off>'.$cli);
+		$this->v('<off>'.$cli);
 
 		$this->shell($this->sudo.$cli,$exit_code);
 
 		return $exit_code;
-	}
-
-	public function e($txt) {
-		echo $this->color($txt).chr(10);
-	}
-
-	public function heading($txt) {
-		$this->e('<cyan>'.str_pad('- '.$txt.' ',exec('tput cols'),'-',STR_PAD_RIGHT).'</cyan>');
-	}
-
-	public function sub_heading($txt) {
-		$this->e('<blue># '.$txt.'</blue>');
-	}
-
-	public function error($txt,$exit=true) {
-		$this->e('<red>'.str_pad('> '.$txt.' ',exec('tput cols'),'<',STR_PAD_RIGHT).'</red>');
-
-		$this->e('<red>    - Task: </off>'.$this->current_task);
-		$this->e('<red>    - Command:  </off>'.$this->current_line);
-
-		if ($exit) {
-			exit(6);
-		}
 	}
 
 	public function merge($input) {
@@ -198,61 +226,18 @@ class deploy {
 		return $input;
 	}
 
-	public function import($filetype,$filepath) {
-		$return = false;
-
-		if (!in_array($filetype,['json','ini','yaml','array'])) {
-			$this->error($filetype.' is a unsupported import type.');
-		}
-
-		if (!file_exists($filepath)) {
-			$this->error('Import could not locate '.$filepath);
-		} else {
-			$this->sub_heading('Importing '.$filepath);
-		}
-
-		switch($filetype) {
-			case 'ini':
-				$array = parse_ini_file($filepath);
-			break;
-			case 'array':
-				$array = require $filepath;
-			break;
-			case 'yaml':
-				if (!function_exists('yaml_parse_file')) {
-					$this->error('yaml_parse_file() not found. Please verify you have the YAML PECL extension installed.');
-				}
-			
-				$array = yaml_parse_file($filepath);
-			break;
-			case 'json':
-				$array = json_decode(file_get_contents($filepath),true);
-			break;
-		}
-
-		if (is_array($array)) {
-			foreach ($array as $name => $value){
-				$this->env[$name] = $value;
-			}
-		} else {
-			$this->error('Your input file did return a Array.');
-		}
-	}
-
 	public function get_deploy() {
-		$deploy_filename = getcwd().'/'.$this->config['deploy_filename'];
-
 		$array = [];
 
-		if (!file_exists($deploy_filename)) {
-			$this->error('Could not locate '.getcwd().'/'.$this->config['deploy_filename'].' file',false);
+		if (!file_exists($this->config['deploy_file'])) {
+			$this->error('Could not locate '.$this->config['deploy_file'].' file',false);
 		} else {
-			$this->sub_heading('Using Deploy File '.getcwd().'/'.$this->config['deploy_filename']);
+			$this->sub_heading('Using Deploy File '.$this->config['deploy_file']);
 
-			$array = json_decode(file_get_contents($deploy_filename));
+			$array = json_decode(file_get_contents($this->config['deploy_file']));
 
 			if ($array === null) {
-				$this->error($this->config['deploy_filename'].' malformed');
+				$this->error($this->config['deploy_file'].' malformed');
 
 				$array = [];
 			}
@@ -309,141 +294,7 @@ class deploy {
 		return proc_close($proc);
 	}
 
-	/** @ switches */
-
-	public function switch_sudo($switch) {
-		$switch = trim($switch);
-
-		if (!$this->switch_storage['sudo setup'] && $switch == 'on') {
-			$this->switch_storage['sudo setup'] = true;
-
-			$this->shell('sudo touch -c acbd18db4cc2f85cedef654fccc4a4d8');
-		}
-
-		$this->sudo = ($switch == 'on') ? 'sudo ' : '';
-	}
-
-	/** add-on commands */
-
-	public function gitx() {
-		$m = __FUNCTION__;
-		$args = func_get_args();
-		$method = array_shift($args);
-	
-		if (method_exists($this,$m.'_'.$method)) {
-			call_user_func_array([$this,$m.'_'.$method],$args);
-		} else {
-			$this->error($m.' function '.$method.' is not found');	
-		}
-	}
-
-	public function gitx_update($path=null,$branch=null) {
-		if (!$branch) {
-			$this->error('GIT Branch not specified please provide one');
-		}
-
-		if (!file_exists($path)) {
-			$this->e('<red>Could not locate directory '.$path.'.</off>');
-		}
-
-		if (!file_exists($path.'/.git')) {
-			$this->e('<red>Not a GIT repository '.$path.'.</off>');
-		} else {
-			$this->e('cd '.$path.';git fetch --all;git reset --hard origin/'.$branch);
-
-			$this->shell('cd '.$path.';git fetch --all;git reset --hard origin/'.$branch);
-		}
-	}
-
-	public function gitx_status($path=null) {
-		if (!file_exists($path)) {
-			$this->e('<red>Could not locate directory '.$path.'.</off>');
-		}
-		
-		exec('find '.$path.' -name FETCH_HEAD',$output);
-
-		$table[] = ['Package','Branch','Hash'];
-
-		foreach ($output as $o) {
-			$dirname = dirname(dirname($o));
-
-			$branch = exec("cd ".str_replace(' ','\ ',$dirname).";git rev-parse --abbrev-ref HEAD");
-			$hash = exec("cd ".str_replace(' ','\ ',$dirname).";git rev-parse --verify HEAD");
-
-			$sections = explode('/',$dirname);
-			$package = end($sections);
-
-			$table[] = [$package,$branch,$hash];
-		}
-
-		$this->table($table);
-	}
-
-	public function gitx_generate($path=null) {
-		if (!file_exists($path)) {
-			$this->e('<red>Could not locate directory '.$path.'.</off>');
-		}
-
-		exec('find '.$path.' -name FETCH_HEAD',$output);
-
-		/* xgit update {PWD} {GIT_BRANCH} */
-		foreach ($output as $o) {
-			$string = 'xgit update # {GIT_BRANCH}';
-			$relative = str_replace($this->config['rootpath'],'{PWD}',dirname(dirname($o)));
-
-			$this->e(str_replace('#',$relative,$string));
-		}
-	}
-
-	public function set($name,$value) {
-		$this->env[$name] = $value;
-	}
-
-	public function capture($name,$shell) {
-		$cmd = $this->merge($this->sudo.$shell);
-		$stdout = '';
-		$stderr = '';
-
-		$error_code = $this->shell($cmd,$stdout,$stderr);
-
-		$this->env[$name] = $stdout;
-
-		return $error_code;
-	}
-
-	public function task($task_name) {
-		if (!$this->task_exists($task_name)) {
-			$this->error("Task \"$task_name\" Not Found.");
-		}
-
-		$this->current_task = $task_name;
-
-		foreach ($this->deploy_json[$task_name] as $command) {
-			$this->current_line = $command;
-			
-			$exit_code = $this->run($command);
-		}
-	}
-
-	public function task_exists($task_name) {
-		return (array_key_exists($task_name,$this->deploy_json));
-	}
-
-	public function selfupdate() {
-		$this->self_update();
-	}
-
-	public function self_update() {
-		$this->heading('Updating Self');
-
-		exec('sudo rm -fdrv /tmp/deploy');
-		exec('sudo git clone https://github.com/dmyers2004/deploy.git /tmp/deploy');
-		exec('sudo mv /tmp/deploy/deploy.php '.__FILE__);
-		exec('sudo chmod -v 755 '.__FILE__);
-
-		$this->sub_heading('Update Complete');
-	}
-
+	/** table */
 	public function table($table) {
 		$widths = [];
 
@@ -501,6 +352,199 @@ class deploy {
 		}
 
 		return $array;
+	}
+
+	/** @ switches */
+
+	public function switch_sudo($switch) {
+		$switch = trim($switch);
+
+		if (!$this->switch_storage['sudo setup'] && $switch == 'on') {
+			$this->switch_storage['sudo setup'] = true;
+
+			$this->shell('sudo touch -c acbd18db4cc2f85cedef654fccc4a4d8');
+		}
+
+		$this->sudo = ($switch == 'on') ? 'sudo ' : '';
+	}
+
+	/** add-on commands */
+
+	public function gitx() {
+		$m = __FUNCTION__;
+		$args = func_get_args();
+		$method = array_shift($args);
+
+		if (method_exists($this,$m.'_'.$method)) {
+			call_user_func_array([$this,$m.'_'.$method],$args);
+		} else {
+			$this->error($m.' function '.$method.' is not found');
+		}
+	}
+
+	public function gitx_update($path=null,$branch=null) {
+		if (!$branch) {
+			$this->error('GIT Branch not specified please provide one');
+		}
+
+		$this->directory_exists($path);
+
+		if (!file_exists($path.'/.git')) {
+			$this->e('<red>Not a GIT repository '.$path.'.</off>');
+		} else {
+			$this->v('cd '.$path.';git fetch --all;git reset --hard origin/'.$branch);
+
+			$this->shell('cd '.$path.';git fetch --all;git reset --hard origin/'.$branch);
+		}
+	}
+
+	public function gitx_status($path=null) {
+		$this->directory_exists($path);
+
+		exec('find '.$path.' -name FETCH_HEAD',$output);
+
+		$table[] = ['Package','Branch','Hash'];
+
+		foreach ($output as $o) {
+			$dirname = dirname(dirname($o));
+
+			$branch = exec("cd ".str_replace(' ','\ ',$dirname).";git rev-parse --abbrev-ref HEAD");
+			$hash = exec("cd ".str_replace(' ','\ ',$dirname).";git rev-parse --verify HEAD");
+
+			$sections = explode('/',$dirname);
+			$package = end($sections);
+
+			$table[] = [$package,$branch,$hash];
+		}
+
+		$this->table($table);
+	}
+
+	public function gitx_generate($path=null) {
+		$this->directory_exists($path);
+
+		exec('find '.$path.' -name FETCH_HEAD',$output);
+
+		/* xgit update {PWD} {GIT_BRANCH} */
+		foreach ($output as $o) {
+			$string = 'xgit update # {GIT_BRANCH}';
+			$relative = str_replace($this->config['rootpath'],'{PWD}',dirname(dirname($o)));
+
+			$this->e(str_replace('#',$relative,$string));
+		}
+	}
+
+	public function set($name,$value) {
+		$this->env[$name] = $value;
+	}
+
+	public function capture($name,$shell) {
+		$cmd = $this->merge($this->sudo.$shell);
+		$stdout = '';
+		$stderr = '';
+
+		$error_code = $this->shell($cmd,$stdout,$stderr);
+
+		$this->env[$name] = $stdout;
+
+		return $error_code;
+	}
+
+	public function task($task_name) {
+		if (!$this->task_exists($task_name)) {
+			$this->error("Task \"$task_name\" Not Found.");
+		}
+
+		$this->current_task = $task_name;
+
+		foreach ($this->deploy_json[$task_name] as $command) {
+			$this->current_line = $command;
+
+			$exit_code = $this->run($command);
+		}
+	}
+
+	public function import($filetype,$filepath) {
+		$return = false;
+
+		if (!in_array($filetype,['json','ini','yaml','array'])) {
+			$this->error($filetype.' is a unsupported import type.');
+		}
+
+		$this->file_exists($filepath);
+
+		$this->sub_heading('Importing '.$filepath);
+
+		switch($filetype) {
+			case 'ini':
+				$array = parse_ini_file($filepath);
+			break;
+			case 'array':
+				$array = require $filepath;
+			break;
+			case 'yaml':
+				if (!function_exists('yaml_parse_file')) {
+					$this->error('yaml_parse_file() not found. Please verify you have the YAML PECL extension installed.');
+				}
+
+				$array = yaml_parse_file($filepath);
+			break;
+			case 'json':
+				$array = json_decode(file_get_contents($filepath),true);
+			break;
+		}
+
+		if (is_array($array)) {
+			foreach ($array as $name => $value){
+				$this->env[$name] = $value;
+			}
+		} else {
+			$this->error('Your input file did return a Array.');
+		}
+	}
+
+	public function heading($txt) {
+		$this->e('<cyan>'.str_pad('- '.$txt.' ',exec('tput cols'),'-',STR_PAD_RIGHT).'</cyan>');
+	}
+
+	public function sub_heading($txt) {
+		$this->e('<blue># '.$txt.'</blue>');
+	}
+
+	public function error($txt,$exit=true) {
+		$this->e('<red>'.str_pad('> '.$txt.' ',exec('tput cols'),'<',STR_PAD_RIGHT).'</red>');
+
+		if ($this->current_task) {
+			$this->e('<red>    - Task: </off>'.$this->current_task);
+		}
+
+		if ($this->current_line) {
+			$this->e('<red>    - Command:  </off>'.$this->current_line);
+		}
+
+		if ($exit) {
+			exit(6);
+		}
+	}
+
+	public function e($txt) {
+		echo $this->color($txt).chr(10);
+	}
+
+	public function v($txt) {
+		if ($this->config['verbose']) {
+			$this->e($txt);
+		}
+	}
+
+	public function file_exists($path,$extra='file ') {
+		if (!file_exists($path)) {
+			$this->error('Could not locate '.$extra.$path);
+		}
+	}
+
+	public function directory_exists($path) {
+		$this->file_exists($path,'directory ');
 	}
 
 } /* end class */
